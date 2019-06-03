d09e4002a5d2dceacb98b8a6f58ca35631341a6a
DanielMartinAlarcon <daniel@martinalarcon.org>
2019-06-03 15:30:57 -0700

    bubble sort done

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -20,7 +20,21 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    
 
+    while True:
+        done = True
+        # Loop all the way through
+        for i in range(1, len(arr)):
+            # If two elements are out of order, swap them
+            if arr[i] < arr[i-1]:
+                temp = arr[i]
+                arr[i] = arr[i-1]
+                arr[i-1] = temp
+                done = False
+        # If you reach the end without swapping, finish
+        if done:
+            break
     return arr
 
 
