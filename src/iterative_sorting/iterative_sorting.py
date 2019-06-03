0ec96a28d9594569398270e478b343d04ff8dd42
DanielMartinAlarcon <daniel@martinalarcon.org>
2019-06-03 16:07:47 -0700

    count_sort done

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,24 +1,24 @@
-# TO-DO: Complete the selection_sort() function below 
+# Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
-        # TO-DO: swap
-
-
-
+        
+        # find next smallest element
+        for j in range(cur_index, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+        
+        # swap current and smallest elements
+        temp = arr[cur_index]
+        arr[cur_index] = arr[smallest_index]
+        arr[smallest_index] = temp
 
     return arr
 
 
-# TO-DO:  implement the Bubble Sort function below
+# implement the Bubble Sort function below
 def bubble_sort( arr ):
     
 
@@ -40,5 +40,13 @@ def bubble_sort( arr ):
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
+    # Check for negative numbers
+    if not all([x > -1 for x in arr]):
+        return "Error, negative numbers not allowed in Count Sort"
+    
+    
 
     return arr
+
+arr3 = [1, 5, -2, 4, 3]
+print(count_sort(arr3))
